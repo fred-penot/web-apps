2108a02f9accacfd1fd4510887c449809f08e79a
fred-penot <frederic.penot@gmail.com>
2017-02-10 20:38:36 +0100

    Update install_sf3.sh

--- install_sf3.sh
+++ install_sf3.sh
@@ -1,7 +1,10 @@
+login_user=docker
+group_user=docker
 cd /home/docker
 /usr/local/zend/bin/php -r "file_put_contents('symfony', file_get_contents('https://symfony.com/installer'));"
 chmod -f 755 symfony
 /usr/local/zend/bin/php symfony new projectSf3
+chown -R ${login_user}:${group_user} /home/docker/projectSf3
 chmod -Rf 777 /home/docker/projectSf3
 ln -s /home/docker/projectSf3 /var/www/html/projectSf3
 cd /home/docker/projectSf3
