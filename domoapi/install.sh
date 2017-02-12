14603dbb3a5a78805b12f44abe4a4e3773974e4f
fred-penot <frederic.penot@gmail.com>
2017-02-12 13:41:41 +0100

    Update install.sh

--- domoapi/install.sh
+++ domoapi/install.sh
@@ -1,25 +1,27 @@
 login_user=docker
 group_user=docker
-cd /home/docker
+
+mkdir -p /home/apps
+cd /home/apps
 git clone https://github.com/fred-penot/DomoApi.git
-mv /home/docker/DomoApi /home/docker/domoapi
-chmod -Rf 777 /home/docker/domoapi
-cd /home/docker/domoapi
+mv /home/apps/DomoApi /home/apps/domoapi
+chmod -Rf 777 /home/apps/domoapi
+cd /home/apps/domoapi
 /usr/local/zend/bin/php composer.phar install
-rm -f /home/docker/domoapi/vendor/knplabs/console-service-provider/Knp/Provider/ConsoleServiceProvider.php
-cp /home/docker/domoapi/app/doc/ConsoleServiceProvider.php /home/docker/domoapi/vendor/knplabs/console-service-provider/Knp/Provider/
-cp -Rf /home/docker/domoapi/app/doc/allocine /home/docker/domoapi/vendor/
-rm -f /home/docker/domoapi/vendor/composer/autoload_psr4.php
-cp /home/docker/domoapi/app/doc/autoload_psr4.php /home/docker/domoapi/vendor/composer/
-rm -f /home/docker/domoapi/vendor/phansys/getid3/GetId3/Lib/Helper.php
-cp /home/docker/domoapi/app/doc/Helper.php /home/docker/domoapi/vendor/phansys/getid3/GetId3/Lib/
-rm -f /home/docker/domoapi/vendor/phansys/getid3/GetId3/GetId3.php
-cp /home/docker/domoapi/app/doc/GetId3.php /home/docker/domoapi/vendor/phansys/getid3/GetId3/
-cd /home/docker/domoapi/app/doc
+rm -f /home/apps/domoapi/vendor/knplabs/console-service-provider/Knp/Provider/ConsoleServiceProvider.php
+cp /home/apps/domoapi/app/doc/ConsoleServiceProvider.php /home/apps/domoapi/vendor/knplabs/console-service-provider/Knp/Provider/
+cp -Rf /home/apps/domoapi/app/doc/allocine /home/apps/domoapi/vendor/
+rm -f /home/apps/domoapi/vendor/composer/autoload_psr4.php
+cp /home/apps/domoapi/app/doc/autoload_psr4.php /home/apps/domoapi/vendor/composer/
+rm -f /home/apps/domoapi/vendor/phansys/getid3/GetId3/Lib/Helper.php
+cp /home/apps/domoapi/app/doc/Helper.php /home/apps/domoapi/vendor/phansys/getid3/GetId3/Lib/
+rm -f /home/apps/domoapi/vendor/phansys/getid3/GetId3/GetId3.php
+cp /home/apps/domoapi/app/doc/GetId3.php /home/apps/domoapi/vendor/phansys/getid3/GetId3/
+cd /home/apps/domoapi/app/doc
 unzip DomoApi.sql.zip
 unzip HomeOS.sql.zip
 rm -f DomoApi.sql.zip
 rm -f HomeOS.sql.zip
-chown -R ${login_user}:${group_user} /home/docker/domoapi
-chmod -Rf 777 /home/docker/projectSf3
-ln -s /home/docker/domoapi /var/www/html/domoapi
+chown -R ${login_user}:${group_user} /home/apps/domoapi
+chmod -Rf 777 /home/apps/projectSf3
+ln -s /home/apps/domoapi /var/www/html/domoapi
