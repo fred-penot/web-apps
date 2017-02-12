59e1c8792c5445d1e01950e6fd89adb0f4cebb85
fred-penot <frederic.penot@gmail.com>
2017-02-12 13:34:41 +0100

    Update services.sh

--- services.sh
+++ services.sh
@@ -8,20 +8,20 @@ service mysql start
 service telldusd start
 
 # Import des bdd de DomoApi...
-if [ -f /var/www/html/domoapi/app/doc/DomoApi.sql ]
+if [ -f /home/docker/domoapi/app/doc/DomoApi.sql ]
 then
-	mysql -u root -p${password_mysql} < /var/www/html/domoapi/app/doc/DomoApi.sql
-	rm -f /var/www/html/domoapi/app/doc/DomoApi.sql
+	mysql -u root -p${password_mysql} < /home/docker/domoapi/app/doc/DomoApi.sql
+	rm -f /home/docker/domoapi/app/doc/DomoApi.sql
 fi
 
 # ... et de HomeOS
-if [ -f /var/www/html/domoapi/app/doc/HomeOS.sql ]
+if [ -f /home/docker/domoapi/app/doc/HomeOS.sql ]
 then
-	mysql -u root -p${password_mysql} < /var/www/html/domoapi/app/doc/HomeOS.sql
-	rm -f /var/www/html/domoapi/app/doc/HomeOS.sql
+	mysql -u root -p${password_mysql} < /home/docker/domoapi/app/doc/HomeOS.sql
+	rm -f /home/docker/domoapi/app/doc/HomeOS.sql
 fi
 
-chmod -Rf 777 /var/www/html/domoapi
+chmod -Rf 777 /home/docker/domoapi
 
 # Suppresion des fichiers temporaires de DomoApi
-rm -Rf /var/www/html/domoapi/app/doc/*
+rm -Rf /home/docker/domoapi/app/doc/*
